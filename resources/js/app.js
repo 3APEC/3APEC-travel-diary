826827d6db1e190495884854a217219a792e22fa
ArturSloyan <sloyan.artur03@gmail.com>
2024-05-24 10:44:19 +0200

    javascript code for hide and show profiloptions, navbar working

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,22 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+document.addEventListener('DOMContentLoaded', function() {
+    const profileButton = document.getElementById('user-menu-button');
+    const profileDropdown = document.getElementById('profile-dropdown');
+  
+    profileButton.addEventListener('click', function(event) {
+        event.stopPropagation();
+        profileDropdown.classList.toggle('hidden');
+        console.log('Profile button clicked.');  // Debugging log
+    });
+  
+    // Close the dropdown if clicked outside
+    window.addEventListener('click', function(e) {
+        if (!profileButton.contains(e.target) && !profileDropdown.contains(e.target)) {
+            profileDropdown.classList.add('hidden');
+            console.log('Clicked outside.');  // Debugging log
+        }
+    });
+  });
